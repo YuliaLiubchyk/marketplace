5bde86db30fe847e3b0a317615e876f37cb8cd5a
Yulia Liubchyk <ciopka@ukr.net>
2019-11-04 08:42:48 +0000

    upload products

--- src/scenes/AddProduct/AddProductView.js
+++ src/scenes/AddProduct/AddProductView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormContainer, FormInput } from '../../components/index';
+import Api from '../../api';
 import s from './AddProduct.module.scss';
 
 function AddProduct() {
