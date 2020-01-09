a5c3bedd39275af0356102b6ee139913b16fba1a
Yuliia Liubchyk <yuliia.liubchyk@dataart.com>
2020-01-09 11:47:26 +0200

    fix login

--- src/components/Input/PasswordInput.jsx
+++ src/components/Input/PasswordInput.jsx
@@ -1,46 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Input.module.scss';
 import { ReactComponent as ShowPassword } from '../../assets/Shape.svg';
 import { ReactComponent as HidePassword } from '../../assets/Hide.svg';
 
-class PasswordInput extends React.Component {
+function PasswordInput({ name, onChange, ...props }) {
+    const [fields, setFields] = useState({ isHidden: true });
+    const toggleShowHide = () => setFields({ ...fields, isHidden: !fields.isHidden });
 
-    constructor({ name, onChange, ...props }) {
-        super();
-        this.state = {
-            name: name,
-            onChange: onChange,
-            hidden: true,
-            properties: props
+    return <div className={s.inputRectangle}>
+        <input {...props}
+            onChange={(e) => onChange(name, e.target.value)}
+            name={name}
+            type={fields.isHidden ? 'password' : 'text'}
+            className={s['password-input']} />
+        {fields.isHidden
+            ? <ShowPassword
+                onClick={toggleShowHide}
+                className={s.show} />
+            : <HidePassword
+                onClick={toggleShowHide}
+                className={s.hide} />
         }
-        this.toggleShow = this.toggleShow.bind(this);
-    }
-
-    toggleShow() {
-        const state = this.state;
-        this.setState({ ...state, hidden: !this.state.hidden });
-    }
-
-    render() {
-        const name = this.state.name;
-        const isHidden = this.state.hidden;
-        return <div className={s.inputRectangle}>
-            <input {...this.state.properties}
-                onChange={(e) => this.state.onChange(name, e.target.value)}
-                name={name}
-                type={isHidden ? 'password' : 'text'}
-                className={s['password-input']} />
-            {isHidden
-                ? <ShowPassword
-                    onClick={this.toggleShow}
-                    className={s.show} />
-                : <HidePassword
-                    onClick={this.toggleShow}
-                    className={s.hide} />}
-
-        </div>
-    }
-
+    </div>
 }
 
 export default PasswordInput;
