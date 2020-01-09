11b79245d9446863733973cb842856c6c7b1d786
Yuliia Liubchyk <yuliia.liubchyk@dataart.com>
2020-01-09 11:59:37 +0200

    small improvement

--- src/components/Input/PasswordInput.jsx
+++ src/components/Input/PasswordInput.jsx
@@ -4,16 +4,16 @@ import { ReactComponent as ShowPassword } from '../../assets/Shape.svg';
 import { ReactComponent as HidePassword } from '../../assets/Hide.svg';
 
 function PasswordInput({ name, onChange, ...props }) {
-    const [fields, setFields] = useState({ isHidden: true });
-    const toggleShowHide = () => setFields({ ...fields, isHidden: !fields.isHidden });
+    const [isHidden, setHidden] = useState(true);
+    const toggleShowHide = () => setHidden(!isHidden);
 
     return <div className={s.inputRectangle}>
         <input {...props}
             onChange={(e) => onChange(name, e.target.value)}
             name={name}
-            type={fields.isHidden ? 'password' : 'text'}
+            type={isHidden ? 'password' : 'text'}
             className={s['password-input']} />
-        {fields.isHidden
+        {isHidden
             ? <ShowPassword
                 onClick={toggleShowHide}
                 className={s.show} />
