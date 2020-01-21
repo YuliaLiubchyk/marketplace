3fd8d4203045a57b53e32f6d196a54983237d569
Yuliia Liubchyk <yuliia.liubchyk@dataart.com>
2020-01-21 13:12:40 +0200

    useContext hook

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FormContext } from '../FormContainer/FormContainer';
 import s from './FormInput.module.scss';
 
-//TODO:: use 'useContext' hook instead of 'Consumer'
 function FormInput({ label, name, ...props }) {
-    return <FormContext.Consumer>
-        {({ formState, onChange }) => (
-            <div>
-                <label>
-                    <div className={s.label}>{label}</div>
-                    <input {...props}
-                        value={formState[name]}
-                        name={name}
-                        onChange={(e) => onChange(name, e)}
-                        className={s.input}/>
-                </label>
-            </div>
-        )}
-    </FormContext.Consumer>
+    const value = useContext(FormContext);
+    return <div>
+        <label>
+            <div className={s.label}>{label}</div>
+            <input {...props}
+                value={value.formState[name]}
+                name={name}
+                onChange={(e) => value.onChange(name, e)}
+                className={s.input} />
+        </label>
+    </div>
 }
 
 export default FormInput;
+
